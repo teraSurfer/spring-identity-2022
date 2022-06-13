7d16c6972a798c222f6e5b063f0a992ea1ee3dc6
teraSurfer <achalaesh@live.com>
2022-06-12 18:31:40 -0700

    init

---
+++ src/main/java/com/example/springidentity2022/SpringIdentity2022Application.java
@@ -0,0 +1,13 @@
+package com.example.springidentity2022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIdentity2022Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringIdentity2022Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springidentity2022/SpringIdentity2022ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springidentity2022;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIdentity2022ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
